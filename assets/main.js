cd5e4d98b75dd3079ba43b6e92057a81184745bb
Sergey <siriusdev3@gmail.com>
2022-03-03 10:28:06 +0300

    Save project

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,38 @@
+let gameDataPath = "/assets/game.json";
+let hostname = window.location.hostname;
+
 $(function () {
-    $.getJSON("/assets/game.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
-    });
+    let gameData;
 
-    function changeScene(scene) {
+    let $sceneView = $('#sceneView');
 
+    loadGameData();
+
+    function loadGameData() {
+        $.getJSON(gameDataPath, function(json) {
+            gameData = json;
+            console.log('gameData', gameData);
+
+
+            changeScene(gameData.defaultLocation);
+        });
+    }
+
+    function changeScene(locationName) {
+        let location = gameData.locations[locationName];
+
+        changeStage(location);
+    }
+
+    function changeStage(location, stageNumber) {
+        let stage = location.stages[location.defaultStage];
+        console.log('stageData', stage);
+
+        $sceneView.addClass("blurred");
+        $sceneView.css("background-image", `url(${stage.background})`);
+        let sceneTimerId = setTimeout(function blurring() {
+            $sceneView.removeClass("blurred");
+        }, 400);
+        console.log( hostname + stage.background);
     }
 });

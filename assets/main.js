9ef7a4c18a0b6f433172a072c9a46a8a9e158ac8
Sergey <siriusdev3@gmail.com>
2022-03-03 06:37:24 +0300

    Remove old project

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $.getJSON("/assets/game.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+
+    function changeScene(scene) {
+
+    }
+});
